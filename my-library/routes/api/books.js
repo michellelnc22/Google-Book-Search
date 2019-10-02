e5de2e0bbf92639f1c60d3d7d8101bd21d4e68d8
michellelnc22 <michellelnc22@gmail.com>
2019-10-01 20:32:27 -0400

    new commit

---
+++ my-library/routes/api/books.js
@@ -0,0 +1,14 @@
+const router = require("express").Router(); 
+const controller = require("../../controllers/controller"); 
+
+router.route("/")
+.get(controller.findAll)
+.post(controller); 
+
+router
+.route("/:id")
+.get(controller.findById)
+.put(controller.update)
+.delete(controller.remove); 
+
+module.exports = router; 
